7b0db9c6d451e57bb96cb75bde588fa2ac833fcb
Damian Bielecki <bielik20@gmail.com>
2017-09-04 17:09:40 +0200

    Add fails

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - https://code.msdn.microsoft.com/How-to-using-MongoDB-with-74f3e1cf#content
 - http://www.qappdesign.com/using-mongodb-with-net-core-webapi/
 - http://www.mattburkedev.com/updating-inside-a-nested-array-with-the-mongodb-positional-operator-in-c-number/
+- http://mongodb.github.io/mongo-csharp-driver/2.0/reference/driver/definitions/#projections
--- aspnetcore-mongodb/Controllers/HomeController.cs
+++ aspnetcore-mongodb/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using aspnetcore_mongodb.Models;
 using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
 
 namespace aspnetcore_mongodb.Controllers
 {
@@ -47,7 +49,7 @@ namespace aspnetcore_mongodb.Controllers
         {
             return View();
         }
-        
+
         public IActionResult Book()
         {
             MongoDBContext dbContext = new MongoDBContext();
@@ -57,7 +59,7 @@ namespace aspnetcore_mongodb.Controllers
             var book = new Book()
             {
                 Title = "Test Book 2",
-                Posts =postList
+                Posts = postList
             };
 
             dbContext.Books.InsertOne(book);
@@ -69,7 +71,39 @@ namespace aspnetcore_mongodb.Controllers
         {
             MongoDBContext dbContext = new MongoDBContext();
 
-            var test = dbContext.Books.AsQueryable<Book>().Where(x => x.Title == "Test Book 1" && x.Posts.Any(p => p.Title == "Updated")).Select(b => b.Posts).FirstOrDefault();
+            //var test = dbContext.Books.AsQueryable<Book>()
+            //            .Where(x => x.Title == "Test Book 1")
+            //            .Select(b => b.Posts.Where(p => p.Title == "No ID Title"))
+            //            .ToList();
+
+            var test = dbContext.Books.Find(b => b.Title == "Test Book 1" && b.Posts.Any(x => x.Title == "Updated"))
+                        .Project(b => b.Posts)
+                        .FirstOrDefault();
+
+            //var filter = Builders<Book>.Filter.Eq("Posts.Title", "Updated");
+            //var projection = Builders<Book>.Projection.Include("Posts.$").Exclude("_id");
+            //var post = dbContext.Books.Find(filter).Project(projection).SingleOrDefault();
+
+            //var query = from b in dbContext.Books.AsQueryable()
+            //            where b.Title == "Test Book 2"
+            //            from p in b.Posts.AsQueryable()
+            //            where p.Title == "No ID Title"
+            //            select p;
+            //var test2 = query.FirstOrDefault();
+
+            //var test3 = dbContext.Books.AsQueryable()
+            //            .SelectMany(b => b.Posts, (b, p) => new { Book = b, Post = p })
+            //            .Where(x => x.Book.Title == "Test Book 2" && x.Post.Title == "Updated")
+            //            .Select(x => x.Post)
+            //            .FirstOrDefault();
+
+            var result = from k in (from a in dbContext.Books.AsQueryable()
+                                    from b in a.Posts
+                                    select b)
+                         where k.Title == "Updated"
+                         select k;
+            var test2 = result.FirstOrDefault();
+
             //var newPost = new Post
             //{
             //    Id = new Guid(),
